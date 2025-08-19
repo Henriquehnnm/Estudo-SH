27ff715b536883455cea7b99ea9b4e7486340878
Henriquehnnm <henriquesardinha987@gmail.com>
2025-08-19 10:46:27 -0300

    Update

--- Login_system/login.sh
+++ Login_system/login.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-# Cria o arquivo de login se ele não existir
+# Cria o TOML
 touch ./login.toml
 
-# ==================================
-# Seção de criação de novo usuário
-# ==================================
+# Sistema de criação de usuário
 if [[ "$1" == "-n" ]]; then
 echo -e "== Bem-vindo! Crie sua conta para continuar =="
 
 read -p "Digite seu nome de usuário: " username
 
-# Verifica se o nome de usuário já existe no arquivo
 if grep -q "^\[user\.${username}\]" login.toml; then
   echo -e "Esse usuário já existe, tente outro nome..."
   exit 1
@@ -22,9 +19,7 @@ echo
 read -s -p "Confirme sua senha: " confirm_password
 echo
 
-# Compara as senhas digitadas
 if [[ "${new_password}" == "$confirm_password" ]]; then
-  # Adiciona o novo usuário e a senha ao arquivo TOML
   password_hash=$(echo -n "$new_password" | sha256sum | awk '{print $1}')
   echo -e "[user.${username}]\npass = ${password_hash}\n" >> ./login.toml
   echo -e "Usuário ${username} criado com sucesso!"
@@ -35,12 +30,9 @@ else
 fi
 fi
 
-# ==================================
-# Seção de login
-# ==================================
+# Sistema de login
 read -p "Digite o nome de usuário: " username
 
-# Encontra a senha armazenada para o usuário digitado
 if grep -q "^\[user\.${username}\]" login.toml; then
   stored_password=$(grep -A1 "\[user.${username}\]" login.toml | tail -n1 | awk -F '=' '{print $2}' | sed 's/^ *//;s/ *$//')
 else
@@ -53,7 +45,6 @@ read -s -p "Digite sua senha: " entered_password
 sp_hash=$(echo -n "$entered_password" | sha256sum | awk '{print $1}')
 echo
 
-# Compara a senha digitada com a senha armazenada
 if [[ "${sp_hash}" == "${stored_password}" ]]; then
   echo -e "Login bem-sucedido! Iniciando..."
   exit 0
